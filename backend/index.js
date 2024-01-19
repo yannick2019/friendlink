e64ced57621763ca79f69bc73301bfe99422341b
Yannick Yanga <cleps2012@gmail.com>
2024-01-19 16:18:55 +0100

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,11 @@ const PORT = process.env.PORT || 8800;
 dbConnection();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["POST", "GET", "PUT"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
