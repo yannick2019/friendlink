eb0f593ffbe05c336ba019cdf30185c60890d724
Yannick Yanga <cleps2012@gmail.com>
2024-01-20 19:27:37 +0100

    __dirname update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -41,11 +41,11 @@ router.post("/profile-view", userAuth, profileViews);
 router.post("/suggested-friends", userAuth, suggestedFriends);
 
 router.get("/verified", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./var/task/views/build", "index.html"));
 });
 
 router.get("/resetpassword", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./var/task/views/build", "index.html"));
 });
 
 export default router;
